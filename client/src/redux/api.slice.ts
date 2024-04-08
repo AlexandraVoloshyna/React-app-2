ec64807a9257c017cfc50504f366d61621bcf389
AlexandraVoloshyna <oleksandravolosyna@gmail.com>
2024-04-08 13:37:05 +0300

    changed base url

--- client/src/redux/api.slice.ts
+++ client/src/redux/api.slice.ts
@@ -1,5 +1,5 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://trello-server-wpjf.onrender.com/api"
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ 
